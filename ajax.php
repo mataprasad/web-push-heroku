bd804644be1f87a7de0be90bfd633611139c6be0
mataprasad045 <mataprasad045@gmail.com>
2017-01-24 23:15:29 +0530

    hitx

---
+++ ajax.php
@@ -0,0 +1,22 @@
+//next example will insert new conversation
+$service_url = 'http://web-push.apphb.com/ajax.aspx';
+$curl = curl_init($service_url);
+$curl_post_data = array(
+        'token' => 'test message'
+);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
+$curl_response = curl_exec($curl);
+if ($curl_response === false) {
+    $info = curl_getinfo($curl);
+    curl_close($curl);
+    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+}
+curl_close($curl);
+$decoded = json_decode($curl_response);
+if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+    die('error occured: ' . $decoded->response->errormessage);
+}
+echo 'response ok!';
+var_export($decoded->response);
